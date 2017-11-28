b4f7ecb821e96be0931fafdc56fc3e6bdaf4de68
Josiah Seaman <josiah@newline.us>
2017-11-28 11:43:25 +0000

    Deleted redundant fasta methods from DDVUtils

--- DNASkittleUtils/DDVUtils.py
+++ DNASkittleUtils/DDVUtils.py
@@ -87,58 +87,6 @@ def first_word(string):
     return re.split('[\W_]+', string)[0]
 
 
-def __do_write(filestream, seq, header=None):
-    """Specialized function for writing sets of headers and sequence in FASTA.
-    It chunks the file up into 70 character lines, but leaves headers alone"""
-    if header is not None:
-        filestream.write(header + '\n')  # double check newlines
-    try:
-        for line in chunks(seq, 70):
-            filestream.write(line + '\n')
-    except Exception as e:
-        print(e)
-
-
-def _write_fasta_lines(filestream, seq):
-    import _io
-    assert isinstance(filestream,
-                      _io.TextIOWrapper)  # I'm actually given a file name and have to open it myself
-    contigs = seq.split('\n')
-    index = 0
-    while index < len(contigs):
-        if len(contigs) > index + 1 and contigs[index].startswith('>') and contigs[index + 1].startswith('>'):
-            print("Warning: Orphaned header:", contigs[index])
-        if contigs[index].startswith('>'):
-            header, contents = contigs[index], contigs[index + 1]
-            index += 2
-        else:
-            header, contents = None, contigs[index]
-            index += 1
-        __do_write(filestream, contents, header)
-
-
-def write_complete_fasta(file_path, seq_content_array, header=None):
-    """This function ensures that all FASTA files start with a >header\n line"""
-    with open(file_path, 'w') as filestream:
-        if seq_content_array[0] != '>':  # start with a header
-            temp_content = seq_content_array
-            if header is None:
-                header = '>%s\n' % just_the_name(file_path)
-            if isinstance(temp_content, list):
-                seq_content_array = [header]
-            else:
-                seq_content_array = array('u', header)
-            seq_content_array.extend(temp_content)
-        _write_fasta_lines(filestream, ''.join(seq_content_array))
-
-
-def write_contigs_to_file(out_filename, contigs):
-    with open(out_filename, 'w') as outfile:
-        for contig in contigs:
-            __do_write(outfile, header='>' + contig.name, seq=contig.seq)
-    print("Done writing ", len(contigs), "contigs and {:,}bp".format(sum([len(x.seq) for x in contigs])))
-
-
 class BlankIterator:
     def __init__(self, filler):
         self.filler = filler
